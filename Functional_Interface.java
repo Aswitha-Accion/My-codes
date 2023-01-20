e3aaae2ac48d2c2f99eb0375c590236fb2ccda75
Aswitha-Accion <119443532+Aswitha-Accion@users.noreply.github.com>
2023-01-20 21:51:25 +0530

    Add files via upload

---
+++ Functional_Interface.java
@@ -0,0 +1,18 @@
+package com.secondjavaprogram;
+
+public class Functional_Interface 
+{
+	@FunctionalInterface
+    public void firstInt() {
+	}
+    public String toString() 
+    {
+		return null;
+    	
+    }
+    public boolean equals(Object obj)
+    {
+		return false;
+    	
+    }
+}
